e68675c286448b4d04beb40a0364749d2c3ec581
Jeremy Hammond <jeremy.hammond@nextcentury.com>
2014-03-12 22:53:09 -0400

    game tracker screen..

--- src/com/hammond/stattracker/ui/PlayGameActivity.java
+++ src/com/hammond/stattracker/ui/PlayGameActivity.java
@@ -1,30 +1,64 @@
 package com.hammond.stattracker.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hammond.stattracker.R;
+import com.hammond.stattracker.domain.Player;
 import com.hammond.stattracker.domain.Team;
 import com.hammond.stattracker.service.PlayerService;
-import com.hammond.stattracker.ui.components.PlayerGameListAdapter;
-
 
 public class PlayGameActivity extends Activity {
 
-	private Team team;
-
-	protected PlayerService playerService;
+	private PlayerService playerService;
 	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play_game);
-        playerService = new PlayerService(this);
-        final ListView listview = (ListView) findViewById(R.id.gameRosterList);
-        
-        this.team = (Team) this.getIntent().getSerializableExtra("team");
-        PlayerGameListAdapter adapter = new PlayerGameListAdapter(this, this.team);
-        listview.setAdapter(adapter);
-    }
+	private Team team;
+	
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		playerService = new PlayerService(this);
+		this.team = (Team) this.getIntent().getSerializableExtra("team");
+		// no more this
+		// setContentView(R.layout.list_fruit);
+
+		//setListAdapter(new ArrayAdapter<String>(this, R.layout.player_game_row_layout,	getPlayerNames()));
+
+		setContentView(R.layout.activity_play_game);
+		
+		
+		ListView listView = (ListView) findViewById(R.id.gameRosterList);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,  android.R.layout.simple_spinner_dropdown_item, this.getPlayerNames());
+		listView.setAdapter(adapter);
+		listView.setTextFilterEnabled(true);
+
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// When clicked, show a toast with the TextView text
+				Toast.makeText(getApplicationContext(),
+						((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+			}
+		});
+
+	}
+	
+	private List<String> getPlayerNames(){
+		List<String> players = new ArrayList<String>();
+		for(Player player : this.playerService.getPlayersForTeam(team)){
+			players.add(player.toString());
+		}
+		return players;
+	}
 }
